4f2a690b3797c15ba2fbc05d7b6b160f99e53b78
lijiamao <951572198@qq.com>
2023-09-06 16:35:12 +0800

    feat：抽离发送信息组件

--- src/components/ConfigChat.vue
+++ src/components/ConfigChat.vue
@@ -16,20 +16,7 @@
         <a-tabs v-model:activeKey="activeType" size="small" tab-position="top" :style="{ width: '360px' }">
           <a-tab-pane v-for="addType in addTypes" :key="addType.value" :tab="addType.label"></a-tab-pane>
         </a-tabs>
-        <a-card size="small" :bordered="true" :title="addTypeName">
-          <a-form :model="formState">
-            <template v-if="activeType === 'text'">
-              <a-textarea v-model:value="formState.phoneBattery" :autoSize="{minRows: 3, maxRows: 6}" />
-              <div class="emojis">
-                <img :src="getAssetsFile(`/emoji/emoji_${i}.png`)" v-for="i in 2" :key="i" alt="">
-              </div>
-            </template>
-          </a-form>
-          <template #actions>
-            <a-button block danger type="link" size="small">清空</a-button>
-            <a-button block type="link" size="small">发送</a-button>
-          </template>
-        </a-card>
+        <GenerateForm :title="addTypeName" :type="activeType" />
         <!-- <p>文本、图片、转账、红包、语音、系统消息、拍一拍、撤回消息、时间</p>
         <p>输入、表情</p>
         <p>拖动上传base64</p>
@@ -38,42 +25,14 @@
         <p>语音时间、是否已读</p>
         <p>系统消息</p> -->
       </a-form-item>
-      <!-- <a-form-item label="聊天背景">
-        <a-upload v-model:file-list="fileList" name="avatar" list-type="picture-card" class="avatar-uploader"
-          :show-upload-list="false" action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-          :before-upload="beforeUpload" @change="handleChange">
-          <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
-          <div v-else>
-            <loading-outlined v-if="loading"></loading-outlined>
-            <plus-outlined v-else></plus-outlined>
-            <div class="ant-upload-text">+</div>
-          </div>
-        </a-upload>
-      </a-form-item> -->
     </a-form>
   </perfect-scrollbar>
 </template>
 
 <script setup>
 import { computed, reactive, ref } from "vue";
-import { models, networkTypes, wifiSignals, phoneSignals } from "@/utils/enum"
-import dayjs from 'dayjs';
 import { getAssetsFile } from "@/utils/utils"
-
-const formState = reactive({
-  model: "apple",
-  darkMode: false,
-  networkType: "wifi",
-  wifiSignal: "3",
-  phoneSignal: "4",
-  phoneTime: dayjs('12:21', 'HH:mm'),
-  isCharging: true,
-  phoneBattery: 60,
-  earphoneMode: true,
-  unreadMessages: 1,
-  chatTitle: "小甜甜",
-  voiceMode: false,
-});
+import GenerateForm from "./common/GenerateForm.vue"
 
 const activeType = ref('text')
 const addTypeName = computed(() => {
@@ -111,42 +70,21 @@ const addTypes = reactive([
 ])
 
 import { message } from 'ant-design-vue';
-function getBase64(img, callback) {
-  const reader = new FileReader();
-  reader.addEventListener('load', () => callback(reader.result));
-  reader.readAsDataURL(img);
-}
 const fileList = ref([]);
-const loading = ref(false);
-const imageUrl = ref('');
 const handleChange = info => {
-  if (info.file.status === 'uploading') {
-    loading.value = true;
-    return;
+  const status = info.file.status;
+  if (status !== 'uploading') {
+    console.log(info.file, info.fileList);
   }
-  if (info.file.status === 'done') {
-    // Get this url from response in real world.
-    getBase64(info.file.originFileObj, base64Url => {
-      imageUrl.value = base64Url;
-      loading.value = false;
-    });
-  }
-  if (info.file.status === 'error') {
-    loading.value = false;
-    message.error('upload error');
+  if (status === 'done') {
+    message.success(`${info.file.name} file uploaded successfully.`);
+  } else if (status === 'error') {
+    message.error(`${info.file.name} file upload failed.`);
   }
 };
-const beforeUpload = file => {
-  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
-  if (!isJpgOrPng) {
-    message.error('You can only upload JPG file!');
-  }
-  const isLt2M = file.size / 1024 / 1024 < 2;
-  if (!isLt2M) {
-    message.error('Image must smaller than 2MB!');
-  }
-  return isJpgOrPng && isLt2M;
-};
+function handleDrop(e) {
+  console.log(e);
+}
 </script>
 
 <style lang="less" scoped>
@@ -156,17 +94,7 @@ const beforeUpload = file => {
   align-items: center;
 }
 
-.emojis {
-  display: flex;
-  flex-wrap: wrap;
-  // justify-content: space-around;
-  max-height: 126px;
-  overflow-y: auto;
-  img {
-    width: 22px;
-    margin: 4px 5px;
-  }
-}
+
 
 .avatar-uploader>.ant-upload {
   width: 128px;
---
+++ src/components/common/GenerateForm.vue
@@ -0,0 +1,64 @@
+<template>
+  <a-card size="small" :bordered="true" :title="title">
+    <a-form :model="formState">
+      <template v-if="type === 'text'">
+        <a-textarea v-model:value="formState.text" :autoSize="{ minRows: 3, maxRows: 6 }" />
+        <!-- <div class="emojis">
+          <img :src="getAssetsFile(`/public/emoji/emoji_${i}.png`)" v-for="i in 2" :key="i" alt="">
+        </div> -->
+      </template>
+      <template v-else-if="type === 'image'">
+        <a-upload-dragger v-model:fileList="fileList" name="file" :multiple="false"
+          action="https://www.mocky.io/v2/5cc8019d300000980a055e76" @change="handleChange" @drop="handleDrop">
+          <p class="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p class="ant-upload-text">点击拖动图片到此区域</p>
+        </a-upload-dragger>
+      </template>
+      <template v-else-if="type === 'transferAccounts'">
+        <a-input v-model:value="formState.phoneBattery" :autoSize="{ minRows: 3, maxRows: 6 }" />
+      </template>
+    </a-form>
+    <template #actions>
+      <a-button block danger type="link" size="small">清空</a-button>
+      <a-button block type="link" size="small">发送</a-button>
+    </template>
+  </a-card>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+import { InboxOutlined } from '@ant-design/icons-vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: "发送类型",
+  },
+  type: {
+    type: String,
+    default: "text",
+  }
+})
+
+const formState = reactive({
+  model: "apple",
+  phoneBattery: 60,
+});
+</script>
+
+<style lang="less" scoped>
+.emojis {
+  display: flex;
+  flex-wrap: wrap;
+  // justify-content: space-around;
+  max-height: 126px;
+  overflow-y: auto;
+
+  img {
+    width: 22px;
+    margin: 4px 5px;
+  }
+}
+</style>
