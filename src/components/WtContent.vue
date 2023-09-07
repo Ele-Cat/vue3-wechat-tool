a53c127f0bfe235720e2f21d293abf52b40ace33
lijiamao <951572198@qq.com>
2023-09-07 17:37:13 +0800

    feat：完善手机模型基础框架

--- src/components/WtContent.vue
+++ src/components/WtContent.vue
@@ -6,12 +6,45 @@
       </div> -->
       <div class="wt-preview">
         <div class="phone-wrap">
-          <div class="phone-scale" :style="{transform: `scale(${phoneScale})`}">
-            <div id="phone" class="phone" :style="{width: phoneWidth + 'px', height: phoneHeight + 'px'}">
-              <div class="phone-bar">状态栏</div>
-              <div class="phone-header">导航栏</div>
-              <div class="phone-body">内容</div>
-              <div class="phone-footer">底部操作</div>
+          <div class="phone-scale" :style="{ transform: `scale(${phoneScale})` }">
+            <div id="phone" class="phone" :style="{ width: phoneWidth + 'px', height: phoneHeight + 'px' }">
+              <div class="phone-bar">
+                <div class="phone-time">15:21</div>
+                <div class="phone-sigle phone-sigle-v4">信号</div>
+                <div class="phone-wifi phone-wifi-s3">wifi</div>
+                <div class="phone-battery">
+                  <span>
+                    <font style="width: 60%;">电量</font> <i></i>
+                  </span>
+                </div>
+              </div>
+              <div class="phone-nav">
+                <div class="phone-nav-left">
+                  <div class="phone-nav-back">返回</div> <span>98</span>
+                </div>
+                <div class="phone-nav-center"><span>
+                    <font>微信对话12</font> <i></i>
+                  </span></div>
+                <div class="phone-nav-right">
+                  <div class="phone-nav-more">更多</div>
+                </div>
+              </div>
+              <div class="phone-body">
+                <perfect-scrollbar>
+                  <p v-for="i in 500" :key="i">这里写内容{{ i }}</p>
+                </perfect-scrollbar>
+              </div>
+              <div class="phone-bottom">
+                <div class="phone-bottom-chat">
+                  <div class="wechat-bottom-icon wechat-voice-icon wechat-voice-say-icon">
+                    语音
+                  </div>
+                  <div class="wechat-input wechat-input-say">按住 说话</div>
+                  <div class="wechat-bottom-icon wechat-emoji-icon">表情</div>
+                  <div class="wechat-bottom-icon wechat-more-icon">更多功能</div>
+                </div>
+                <div class="phone-bottom-bar"><i>返回桌面</i></div>
+              </div>
             </div>
           </div>
         </div>
@@ -53,30 +86,93 @@ watch(() => useSystemStore.appearance.model, (newVal) => {
   justify-content: space-around;
   height: 100%;
   padding: 10px;
+
   .wt-skeleton {
     width: 40%;
     padding: 10px;
     background-color: #F1F1F1;
   }
+
   .wt-preview {
     position: relative;
+
     .phone-wrap {
       width: 360px;
       height: 100%;
+
       // background-color: #E0E0E0;
       .phone-scale {
         transform-origin: 0 0;
       }
+
       .phone {
         background-color: #EDEDED;
         position: relative;
         font-size: 36px;
+
         .phone-bar {
           height: 132px;
+          display: flex;
+          align-items: center;
+          padding: 0 43px 0 95px;
+          position: relative;
+          z-index: 99999;
+
+          .phone-time {
+            font-size: 45px;
+            flex: 1;
+            font-weight: 600;
+          }
         }
-        .phone-header {
+
+        .phone-nav {
           height: 132px;
           position: relative;
+          z-index: 99999;
+          display: flex;
+          align-items: center;
+          padding: 0 50px;
+
+          .phone-nav-left {
+            width: 150px;
+            position: relative;
+
+            .phone-nav-back {
+              width: 27px;
+              height: 52px;
+              background: url(@/assets/images/top/wechat-nav-back.png) no-repeat;
+              text-indent: -9999px;
+            }
+
+            span {
+              position: absolute;
+              font-size: 36px;
+              height: 72px;
+              line-height: 72px;
+              background: #d5d5d5;
+              min-width: 72px;
+              border-radius: 36px;
+              text-align: center;
+              padding: 0 24px;
+              top: 50%;
+              margin-top: -36px;
+              left: 45px;
+              font-weight: 600;
+            }
+          }
+
+          .phone-nav-center {
+            flex: 1;
+            display: flex;
+            justify-content: center;
+          }
+
+          .phone-nav-right {
+            width: 150px;
+            display: flex;
+            justify-content: flex-end;
+          }
+
           &::after {
             content: '';
             position: absolute;
@@ -88,6 +184,7 @@ watch(() => useSystemStore.appearance.model, (newVal) => {
             bottom: 2px;
           }
         }
+
         .phone-body {
           position: absolute;
           top: 264px;
@@ -97,36 +194,90 @@ watch(() => useSystemStore.appearance.model, (newVal) => {
           -webkit-overflow-scrolling: touch;
           overflow-y: scroll;
         }
-        .phone-footer {
+
+        .phone-bottom {
           background: #f6f6f6;
           position: absolute;
           bottom: 0;
           width: 100%;
           height: 269px;
+          .phone-bottom-chat {
+            height: 167px;
+            display: flex;
+            align-items: center;
+            .wechat-bottom-icon {
+              width: 80px;
+              height: 80px;
+              text-indent: -9999px;
+              &.wechat-voice-say-icon {
+                background: url(@/assets/images/bottom/wechat-bottom-icon4.png) no-repeat;
+              }
+              &.wechat-voice-icon {
+                background: url(@/assets/images/bottom/wechat-bottom-icon1.png) no-repeat;
+                margin-left: 32px;
+              }
+              &.wechat-emoji-icon {
+                background: url(@/assets/images/bottom/wechat-bottom-icon2.png) no-repeat;
+              }
+              &.wechat-more-icon {
+                background: url(@/assets/images/bottom/wechat-bottom-icon3.png) no-repeat;
+                margin: 0 32px 0 39px;
+              }
+            }
+            .wechat-input-say {
+              color: #1a1a1a;
+              font-size: 48px;
+              text-indent: 0;
+              text-align: center;
+              line-height: 120px;
+              font-weight: 500;
+            }
+            .wechat-input {
+              height: 120px;
+              background: #fff;
+              border-radius: 15px;
+              flex: 1;
+              text-indent: -9999px;
+              margin: 0 32px;
+            }
+          }
+          .phone-bottom-bar {
+            height: 102px;
+            display: flex;
+            justify-content: center;
+            i {
+              width: 402px;
+              height: 15px;
+              background: url(@/assets/images/bottom/phone-bottom-bar.png) no-repeat;
+              margin-top: 63px;
+              text-indent: -9999px;
+            }
+          }
         }
       }
     }
+
     .wt-ctrl {
       position: absolute;
       top: 50%;
       right: -66px;
       width: 60px;
       transform: translate(0, -50%);
+
       .wtc-button {
         height: 60px;
         line-height: 60px;
         text-align: center;
         font-size: 12px;
-        background-image: linear-gradient( 135deg, #FFE985 10%, #FA742B 100%);
-        background-image: linear-gradient( 135deg, #FFA8A8 10%, #FCFF00 100%);
-        background-image: linear-gradient( 135deg, #3B2667 10%, #BC78EC 100%);
-        background-image: linear-gradient( 135deg, #F6D242 10%, #FF52E5 100%);
-        background-image: linear-gradient( 135deg, #FD6E6A 10%, #FFC600 100%);
+        background-image: linear-gradient(135deg, #FFE985 10%, #FA742B 100%);
+        background-image: linear-gradient(135deg, #FFA8A8 10%, #FCFF00 100%);
+        background-image: linear-gradient(135deg, #3B2667 10%, #BC78EC 100%);
+        background-image: linear-gradient(135deg, #F6D242 10%, #FF52E5 100%);
+        background-image: linear-gradient(135deg, #FD6E6A 10%, #FFC600 100%);
         border-radius: 50%;
         margin-bottom: 12px;
         color: #FFFFFF;
       }
     }
   }
-}
-</style>
+}</style>
