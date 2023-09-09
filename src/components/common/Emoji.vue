e5ddfb2eaea89c957d6a9c6cff6448580d7e9460
ele-cat <951572198@qq.com>
2023-09-09 14:44:20 +0800

    fix：修复emoji问题

--- src/components/common/Emoji.vue
+++ src/components/common/Emoji.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="emoji_wrap" style="margin-top: 10px">
-    <a v-for="(emoji, index) in emojis" :key="index" :style="{backgroundImage: `url(/src/assets/emoji/emoji_${index + 1}.png)`}" :title="emoji" @click="handleEmojiClick(emoji)">{{ emoji }}</a>
+    <a v-for="(emoji, index) in emojis" :key="index" :style="{backgroundImage: `url(../../assets/emoji/emoji_${index + 1}.png)`}" :title="emoji" @click="handleEmojiClick(emoji)">
+    <!-- <a v-for="(emoji, index) in emojis" :key="index" :title="emoji" @click="handleEmojiClick(emoji)"> -->
+      <!-- <img :src="`/src/assets/emoji/emoji_${index + 1}.png`" alt=""> -->
+      <p>{{ emoji }}</p>
+    </a>
   </div>
 </template>
 
@@ -23,11 +27,17 @@ const handleEmojiClick = (emoji) => {
     width: 24px;
     height: 24px;
     margin: 4px 5px;
-    // background-image: url(@/assets/emoji/emoji_1.png) !important;
+    background-image: url(@/assets/emoji/emoji_1.png) !important;
     background-size: 24px 24px;
     background-repeat: no-repeat;
-    text-indent: -9999px;
-    color: red;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+    p {
+      text-indent: -9999px;
+      color: red;
+    }
   }
 }
 </style>
