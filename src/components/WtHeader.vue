41c8fbb3f0fde3ae9737484171445147b20e6bb0
lijiamao <951572198@qq.com>
2024-07-12 15:35:04 +0800

    🚀优化顶部样式、新增visitors

--- src/components/WtHeader.vue
+++ src/components/WtHeader.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="wt-header">
     <p data-text="微信对话生成器">微信对话生成器</p>
+    <img src="https://visitor-badge.laobi.icu/badge?page_id=vue3-wechat-tool.visitor-badge&right_color=%23fd6585" alt="">
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 
 <style lang="less" scoped>
 .wt-header {
+  position: relative;
   width: 100%;
   height: 100%;
   background-image: radial-gradient(
@@ -19,62 +21,67 @@
     #455a64 100%
   );
   display: flex;
-}
 
-p {
-  position: relative;
-  margin: auto;
-  font-size: 32px;
-  letter-spacing: 0.2em;
-  display: inline-block;
-  font-weight: bold;
-  white-space: nowrap;
-  color: transparent;
-  background-color: var(--theme-color);
-  background-clip: text;
-  -webkit-background-clip: text;
-}
+  p {
+    position: relative;
+    margin: auto;
+    font-size: 32px;
+    letter-spacing: 0.2em;
+    display: inline-block;
+    font-weight: bold;
+    white-space: nowrap;
+    color: transparent;
+    background-color: var(--theme-color);
+    background-clip: text;
+    -webkit-background-clip: text;
+    &::after {
+      content: attr(data-text);
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      z-index: 5;
+      background-image: linear-gradient(
+        120deg,
+        transparent 0%,
+        transparent 6rem,
+        white 11rem,
+        transparent 11.15rem,
+        transparent 15rem,
+        rgba(255, 255, 255, 0.3) 20rem,
+        transparent 25rem,
+        transparent 27rem,
+        rgba(255, 255, 255, 0.6) 32rem,
+        white 33rem,
+        rgba(255, 255, 255, 0.3) 33.15rem,
+        transparent 38rem,
+        transparent 40rem,
+        rgba(255, 255, 255, 0.3) 45rem,
+        transparent 50rem,
+        transparent 100%
+      );
+      background-clip: text;
+      -webkit-background-clip: text;
+      background-size: 150% 100%;
+      background-repeat: no-repeat;
+      animation: shine 8s infinite linear;
+    }
+  }
 
-p::after {
-  content: attr(data-text);
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 5;
-  background-image: linear-gradient(
-    120deg,
-    transparent 0%,
-    transparent 6rem,
-    white 11rem,
-    transparent 11.15rem,
-    transparent 15rem,
-    rgba(255, 255, 255, 0.3) 20rem,
-    transparent 25rem,
-    transparent 27rem,
-    rgba(255, 255, 255, 0.6) 32rem,
-    white 33rem,
-    rgba(255, 255, 255, 0.3) 33.15rem,
-    transparent 38rem,
-    transparent 40rem,
-    rgba(255, 255, 255, 0.3) 45rem,
-    transparent 50rem,
-    transparent 100%
-  );
-  background-clip: text;
-  -webkit-background-clip: text;
-  background-size: 150% 100%;
-  background-repeat: no-repeat;
-  animation: shine 5s infinite linear;
+  img {
+    position: absolute;
+    left: calc(50% + 144px);
+    bottom: 6px;
+  }
 }
 
 @keyframes shine {
   0% {
-    background-position: 190% 0;
+    background-position: 290% 0;
   }
   100% {
-    background-position: -190% 0;
+    background-position: -290% 0;
   }
 }
 </style>
